2370c4e2354415e0928c1fac01e81feb1a2b64de
serenac420 <144480398+serenac420@users.noreply.github.com>
2023-11-20 14:01:21 -0500

    Update booty.py

--- booty.py
+++
@@ -1,5 +0,0 @@
-import json
-## Open the JSON file of pokemon data
-pokedex = open("./pokedex.json", encoding="utf8")
-## create variable "data" that represents the enitre pokedex list
-data = json.load(pokedex)
