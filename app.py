ff4fae500edaf254ba2a1f1d97cb7080c56df861
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-11-20 13:59:50 -0500

    worked on inventory for enemies

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+test = open("data.json", encoding="utf8")
+data = json.load(test)
+
+def allenemiesEncounters():
+    for i in range (6):
+        everything = (list(data[i].values())) # all values in data
+        print (everything[0])
+allenemiesEncounters()
+
+def allenemiesSEARCH():
+    search = input("What do you wish to search for? (Enemy Description, Enemy Stats): ")
+    if search == ("desc"): 
+        enemy = input("Search for an enemy: ")
+        for i in range (6):
+            everything = (list(data[i].values())) # all values in data
+            if enemy == (everything[0]):
+                print (f"{everything[0]}: {everything[5]}")
+    elif search == ("stats"):
+        enemy = input ("Search for an enemy: ")
+        for i in range (6):
+            everything = (list(data[i].values())) # all values in data
+            if enemy == (everything[0]):
+                print (f"{everything[0]}: Strength: {everything[3]}/10, Health: {everything[-1]}")
+
+allenemiesSEARCH()
