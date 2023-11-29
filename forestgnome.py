8d95d41e2b04a320f0ec79d106403af80e55b2c3
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-11-29 14:01:22 -0500

    worked

--- forestgnome.py
+++ forestgnome.py
@@ -4,7 +4,12 @@ class Forestgnome:
         self.personality = personality
         self.riddle = riddle
         self.chances = chances
+<<<<<<< Updated upstream
     def chances(self, item):
         self.chances.remove(item)
+=======
+    def answer(self):
+        self.chances.remove(self.chances[-1])
+>>>>>>> Stashed changes
         print (f"You have {self.chances} left!!!")
 
