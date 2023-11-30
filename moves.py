35e2d478dd348a91fa00307d6411acaa65503efb
serenac420 <144480398+serenac420@users.noreply.github.com>
2023-11-30 13:59:23 -0500

    h

--- moves.py
+++ moves.py
@@ -3,14 +3,14 @@ import json
 pokedex = open("./move.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
-print(data[0])
+
 
 user_input = input("Enter a move: ")
 match_found = False
 
 #iterating
 for move in data:
-    if move["move","desc"].lower() == user_input.lower():
+    if move["move"].lower() == user_input.lower():
         match_found = True
         print(f"Move found: {move['move']} - {move['desc']}")
 
