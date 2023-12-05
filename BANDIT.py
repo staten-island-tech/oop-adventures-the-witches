1fe591f5c506e54dc7b3f04171e1a3e37cee78fd
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-12-05 13:58:52 -0500

    worked

--- BANDIT.py
+++ BANDIT.py
@@ -1,18 +1,30 @@
 class Bandit:
-    def __init__(self, name, health, personality, weapon, inventorySTEAL):
+    def __init__(self, name, health, personality, weapon, inventory, inventorySTEAL):
         self.name = name
         self.health = health
         self.personality = personality
         self.weapon = weapon
         self.inventorySTEAL = inventorySTEAL
-    def sneaky(self, MCitems, MCinventory):
+        self.inventory = inventory
+    def sneaky(self, MCitems, MCinventorybefore, MCinventoryafter): #MCinventoryafter does not have MCitems
         print ("Whats that sound? I thought I heard something...")
         self.inventorySTEAL.append(MCitems)
-        print (MCinventory.remove(MCitems))
+        print (f"Inventory: \n{MCinventorybefore.replace(MCinventorybefore, MCinventoryafter)}")
         print (f"You're missing {MCitems}!!!")
-        run = input ("Uh oh, should you run after them?")
+        run = input ("Uh oh, do you want to run after them?")
         if run == ("No"):
             print ("Okay..you might not see those items again...")
         elif run == ("Yes"):
             print ("Be prepared for battle!")
-            
+    def battleoutcome(self, MCitems, MCinventory):
+        if self.health <= 0:
+            print (f"{self.name} has been defeated. ")
+            print (f"{self.weapon} has been dropped. ")
+            print (f"You won {MCitems} back!!")
+            MCinventory.append(MCitems)
+            self.inventorySTEAL.replace(self.inventorySTEAL, self.inventory)
+            print (f"Oh, whats this? {self.inventorySTEAL} has been dropped. ")
+        elif self.health > 0:
+            print (f"{self.name} won the battle!")
+            print (f"{self.name}: I guess I'm keeping all these items!")
+    
--- GOBLIN.py
+++ GOBLIN.py
@@ -7,11 +7,15 @@ class Goblin:
         self.personality = personality
         self.weapon = weapon
         self.tricks = tricks
-    def battleoutcome(self):
+    def treasure(self, MCshinyitem, MCinventory):
+        print (f"{self.name}: What is that...")
+        self.inventoryTREASURE.append(MCshinyitem)
+        MCinventory.remove(MCshinyitem)
+    def battleoutcome(self, MCshinyitem):
         if self.health <= 0:
             print (f"{self.name} has been defeated. ")
             print (f"{self.weapon} has been dropped. ")
-            print (f"Oh, whats this? {self.inventoryTREASURE} has been dropped. ")
+            print (f"Oh, whats this? {self.inventoryTREASURE} has been dropped. Turns out {self.name} had stolen {MCshinyitem}!")
         elif self.health > 0:
             print (f"{self.name} won the battle!")
-            print ("Hehehe, I knew I would win !")
+            print (f"{self.name}: Hehehe, I knew I would win! Guess you're not getting {MCshinyitem} back!")
