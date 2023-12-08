164ae17ae8e2d8b9c9f168b4bf4855d36c6fd0f9
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-12-08 13:59:17 -0500

    dfgfdgdfg
    fdgfdg

--- Practice.py/Classes.py
+++ Practice.py/Classes.py
@@ -42,7 +42,7 @@ class NotSuspicious(Guest) :
 
 Guests =[]
 SuspicionS =[]
-NotSuspiciouS =[]
+NotSuspiciousS =[]
 
 
 
@@ -70,8 +70,9 @@ def Create_Suspicions(Name,Occupation,Relation_To_Mansion_owner,Reason_For_Suspi
 def Create_Trustworthy(Name,Occupation,Relation_To_U,ReasonOfTrust,Befriend) :
     Occupation = (uuid.uuid4())
     Trust = NotSuspicious(Name,Occupation,Relation_To_U,ReasonOfTrust,Befriend)
-    NotSuspiciouS.append(Trust)
-    for Trust in NotSuspiciouS :
+    NotSuspiciousS.append(Trust)
+    for Trust in NotSuspiciousS :
+        print("Here Is your Current Suspicions")
         print(Trust)
 
 
@@ -91,49 +92,33 @@ def check_tenure(status):
 while add_users_Guest_Log == "Y" :
     print("Welcome to the journal. Here you will recod all of your findings on the locals.")
     User_Request = input("Before you Input information on this character, Are you Suspicious of Him/Her?(YES or NO or UNDETERMINED)")
+    
     if User_Request.upper() == "YES" :
         print("Classing into Suspicions file...")
         Name = input("Enter A Guest Name.")
         Occupation = input("Please Enter This Guest's Occupation. (Just put a '?' If its unkown.)")
         LastSeenWith = input("Please State Where you last saw this person ")
-        Relation_To_U = input("What is this person's relation to you? Leave a '?' If its unkown.")
+        Relation_To_Mansion_owner= input("What is this person's relation to the mansion owner? Leave a '?' If its unkown.")
         Reason_For_Suspicion = input("Please State in 1-2 EXTREMELY short sentences your reason for suspicion.")
 
         
-        Create_Suspicions(Name,Occupation,Relation_To_U,Reason_For_Suspicion) 
-
+        Create_Suspicions(Name,Occupation,Relation_To_Mansion_owner,Reason_For_Suspicion) 
+###############################################################
     elif User_Request.upper() =="UNDETERMINED" :
         print("Redirecting To Regular Guest Log...")
         Name = input("Enter A Guest Name.")
         Occupation = input("Please Enter This Guest's Occupation. (Just put a '?' If its unkown.)")
         Relation_To_U = input("What is this person's relation to you? Leave a '?' If its unkown.")
-    
+                                            
+###############################################################
         
+    elif User_Request.upper() == "NO" :
+        print("Redirecting to the list of trustworthy people....")
+        Name = input("Enter A Guest Name.")
+        Occupation = input("Please Enter This Guest's Occupation. (Just put a '?' If its unkown.)")
        
-        print("Here is your current guest log.")
-        print(NotSuspiciouS)
+###################################################################
 
 
-print("Welcome to your persnal Journal. Here you will record your suspiciouns abt others")
-
-
-
-class Guest :
-    def __init__(self, Name, Occupation, Relation_To_You, Relation_To_Mansion_owner):
-        self.Name = Name
-        self.Occupation = Occupation
-        self.Relation_To_You = Relation_To_You
-        self.Relation_To_Mansion_owner = Relation_To_Mansion_owner
-       
-
-class Journal(Guest) :
-    def __init__(self,Name,Occupation,Reason_For_Suspicion,Last_Seen_With) :
-         super().__init__(Occupation,Name)
-      
-         self.Reason_For_Suspicion = Reason_For_Suspicion
-         self.Last_Seen_With = Last_Seen_With
-    def __str__(self):
-        return f"{self.Name},{self.Occupation}"
-        
 
 
