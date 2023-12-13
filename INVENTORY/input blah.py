d5f28813dedbd3f0bc329aea0c28454a34e2a780
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2023-12-13 14:00:16 -0500

    vcbx
    xfghfghg

--- INVENTORY/input blah.py
+++
@@ -1,3 +0,0 @@
-from SHOP import Seller
-from TOWNSFOLK import Townfolk
-
---
+++ Practice.py/Proj_Fr.py
@@ -0,0 +1,49 @@
+import uuid
+
+print("This is your journal. Here you will keep a log of the townsfolk. This will help you identify whos the which faster. ")
+
+
+class Townsfolks :
+    def __init__(self,id,Name,Occupation) :
+    
+        self.Name = Name
+        self.Occupation = Occupation
+        self.id = id
+
+"""new_id = str(uuid.uuid4())
+mark = User(new_id, "Mark") ##new instance of User
+print(mark)"""
+
+
+
+
+class Suspect(Townsfolks) :
+    def init__(self,id,Name,Occupation, Last_Seen) :
+        super().__init__(id,Name,Occupation)
+        self.Last_Seen = Last_Seen
+    def __str__(self):
+        return f"{self.Name},{self.Occupation},{self.Last_Seen}"
+    
+
+
+TownsLog =[]
+SuspectList =[]
+
+
+def Create_Towns_Log(Name, Occupation) :
+    id = str(uuid.uuid4())
+
+
+    New_Villager = Townsfolks(id,Name,Occupation)
+    TownsLog.append(New_Villager)
+    for Townsfolk in TownsLog :
+        print(Townsfolk)
+
+
+
+add_more_users = "Y"
+def check_tenure(status):
+    if status.lower() == "y":
+        return True
+    else:
+        return False
