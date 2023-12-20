6ed9123728a6f11105e38ddc0f79cf03d90fdb93
ninac13 <144480538+ninac13@users.noreply.github.com>
2023-12-20 13:58:08 -0500

    worked

--- GAME.py
+++ GAME.py
@@ -1,45 +1,24 @@
 from GNOME import Forestgnome
 from MAINCHARACTER import Maincharacter
 from BANDIT import Bandit
+from GOBLIN import Goblin
 
 mcNAME = input("START OF WITCH COVEN ADVENTURE \nWhat is your name? ")
 papagnome = Forestgnome("Papa Forest Gnome", 250, "What is seen in the middle of March and April that can't be seen at the beginning or end of either month? ", "r", 3, "Wooden Cane")
 mc = Maincharacter(mcNAME, 1000, ["Wooden Cleaning Broom"])
 bandit = Bandit("???", 375, "Knife", [], ["Healing Potion", "Shield Potion", "Tommy Gun","Croissant"])
-jaffrey = 
+# jaffrey = Goblin("Jaffrey", 325, ["Gold Trophy"], "Claws", ""
 
 def Start_Gnome():
-    roadsChoice = input("It was a dark stormy evening. \nAs a young maiden you have just finished work. \nTired and annoyed of the rich family you work for, you slowly head down the wet path leading up to the hill. \nYou see a sign that reads 'Left: Wickery Hills', 'Right: Crystal Cliffs'. \nWhich way do you go? ")
-    if roadsChoice == ("Left"):
-        wickery_hills = input("You head up the path towards Wickery Hills. However you don't seem to remember this path. Go back? ")
-        if wickery_hills == ("Yes"):
-            investigate = input("Wait...This isn't the way I came from.... \n*Sound of leaves rustling.* \nWould you like to investigate the noise? ")
-            if investigate == ("Yes"):
-                print("You begin to approach the bushes...")
-                riddle = papagnome.ask()
-                print(riddle)
-                if papagnome.health == 0:
-                    gnomeBATTLEresult = papagnome.battleresult()
-                    print (gnomeBATTLEresult)
-                    mcVICTORY = mc.victory(papagnome.weapon)
-                    print (mcVICTORY)
-<<<<<<< HEAD
-                # elif papagnome.health > 0:
+    start = papagnome.start()
+    print(start)
 
-=======
-                elif papagnome.health > 0:
-                    run = input(f"{papagnome.name}: Hahahaha...I knew a lowly human like yourself would not be able to answer!\nDo you wish to run away or fight? ")
-                    if run == ("Run"):
-                        print("You run away towards Crystal Cliffs.")
-                    elif run == ("Fight"):
-                        print("jaskdf") #import moves and battle
->>>>>>> 44e79d29b29bc8b2113e38c95c30ab5812bcc886
-            elif investigate == ("No"):
-                print ("You ignore the noise and continue walking.\nWait a second...\nIs that the entrance to Crystal Cliffs?\nYou really don't know your way around...")
-        elif wickery_hills == ("No"):
-            print("You continue walking.\nHuh...you really don't remember this place...\nWait is that the entrance to Crystal Cliffs?")
-    if roadsChoice == ("Right"):
-        print ("You head up the path towards Crystal Cliffs.")
+    if papagnome.health == 0:
+        gnomeBATTLEresult = papagnome.battleresult()
+        print (gnomeBATTLEresult)
+        mcVICTORY = mc.victory(papagnome.weapon)
+        print (mcVICTORY)
+
 Start_Gnome()
 
 def Part2Bandit():
@@ -51,5 +30,5 @@ def Part2Bandit():
     print (banditBATTLE)
 Part2Bandit()
 
-def Part3Goblin():
-    print
+# def Part3Goblin():
+#     print
--- GNOME.py
+++ GNOME.py
@@ -6,13 +6,32 @@ class Forestgnome:
         self.riddleanswer = riddleanswer
         self.chances = chances
         self.weapon = weapon
-    def ask(self):
-        riddle = input(f"{self.name}: {self.riddle}")
-        if riddle == self.riddleanswer:
-            self.health = 0
-            return (f"{self.name}: NOOOO!!! No one has ever answered correctly before!!")
-        elif riddle != self.riddleanswer:
-            return ("Be prepared for battle!!!")
+    def start(self):
+        roadsChoice = input("It was a dark stormy evening. \nAs a young maiden you have just finished work. \nTired and annoyed of the rich family you work for, you slowly head down the wet path leading up to the hill. \nYou see a sign that reads 'Left: Wickery Hills', 'Right: Crystal Cliffs'. \nWhich way do you go? ")
+        if roadsChoice == ("Left"):
+            wickery_hills = input("You head up the path towards Wickery Hills. However you don't seem to remember this path. Go back? ")
+            if wickery_hills == ("Yes"):
+                investigate = input("Wait...This isn't the way I came from.... \n*Sound of leaves rustling.* \nWould you like to investigate the noise? ")
+                if investigate == ("Yes"):
+                    print("You begin to approach the bushes...")
+                    riddle = input(f"{self.name}: {self.riddle}")
+                    if riddle == self.riddleanswer:
+                        self.health = 0
+                        return (f"{self.name}: NOOOO!!! No one has ever answered correctly before!!")
+                    elif riddle != self.riddleanswer:
+                        print ("Be prepared for battle!!!")
+                        run = input(f"{self.name}: Hahahaha...I knew a lowly human like yourself would not be able to answer!\nDo you wish to run away or fight? ")
+                        if run == ("Run"):
+                            return("You run away towards Crystal Cliffs.")
+                        elif run == ("Fight"):
+                            return() #<--import moves and battle
+                elif investigate == ("No"):
+                    return ("You ignore the noise and continue walking.\nWait a second...\nIs that the entrance to Crystal Cliffs?\nYou really don't know your way around...")
+            elif wickery_hills == ("No"):
+                return("You continue walking.\nHuh...you really don't remember this place...\nWait is that the entrance to Crystal Cliffs?")
+        elif roadsChoice == ("Right"):
+            return ("You head up the path towards Crystal Cliffs.")
+    
     def battleresult(self):
         if self.health <= 0:
             print (f"{self.name} has been defeated.")
