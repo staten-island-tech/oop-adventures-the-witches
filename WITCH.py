fdc6416996cf27aa68e476b74b61c9c21d2da3eb
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-01-05 13:20:52 -0500

    worked

--- WITCH.py
+++ WITCH.py
@@ -3,7 +3,6 @@ class Witch:
         self.name = name
         self.health = health
         self.weapon = weapon
-<<<<<<< HEAD
     def battle(self, MCinventory):
         print("After the rain had cleared, you were back on your journey...")
         print("Days later you have arrived at the Witch's Coven...\nYou head in, and there's the witch...waiting for you....\n")
@@ -16,20 +15,11 @@ class Witch:
         if weapon == ("Wooden Broom") or ("Wooden Cane"):
             move = input("What move do you do? (Slash, Stab, ")
             
-=======
-<<<<<<< HEAD
-        self.power = power
-        self.witchinventory = witchinventory
-
-
-    def battleoutcome(self):
-=======
     def Start(self):
         print("After the rain had cleared, you were back on your journey...")
         print("Days later you have arrived at the Witch's Coven...\nYou head in, and there's the witch...waiting for you....\n")
         ###import battle
->>>>>>> 7c8e13db1a33787cbf450023c49b692f263fd225
->>>>>>> 4edc7c4ab83b978e1637086aabd944595daf1a3e
+
         if self.health <= 0:
             print (f"{self.name} has been defeated. ")
             print (f"{self.weapon} has been dropped. ")
