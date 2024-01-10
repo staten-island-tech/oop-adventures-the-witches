eb2f0b58b2e992aeb8406239265538585a97acdc
serenac420 <144480398+serenac420@users.noreply.github.com>
2024-01-10 13:59:02 -0500

    e

--- GNOME.py
+++ GNOME.py
@@ -1,8 +1,12 @@
 import json
 w = open("./weapon.json", encoding="utf8")
 weapons = json.load(w)
-m = open("./moves.json", encoding = "utf8")
-moves = json.load(m)
+
+class Move:
+    def __init__(self, name, description, dmg_amt):
+        self.name = name
+        self. description = description
+        self.dmg_amt = dmg_amt
 
 class Forestgnome:
     def __init__(self, name, health, riddle, riddleanswer, chances, weapon):
@@ -31,33 +35,28 @@ class Forestgnome:
                             return("You run away towards Crystal Cliffs Village.")
                         elif run == ("Fight"):
                             return(f"{self.name} wants to fight!")
-                        fight = input("Which weapon (and) items will you choose? Weapon:")
-                        match_found = False
-                        for weapon in MC.inventory:
-                             if weapon["item"]["name"].lower() == fight.lower():
-                        match_found = True 
-                        print(f"Weapon found: {weapon['name']}")
-                        if not match_found:
-                            return("That is not in your inventory.")
-                            ##actual fighting soon
-                        print(f"{self.name} will attack first. Select a move: 1{self.move} or 2{self.move}.")
-                        return({self.health} - {self.damage}.append)
-                    print(f"{self.name} now has {self.health}")
-                    return("That is not a proper response.")
-                else:
-                        return("That is not a proper response.")
-            elif investigate == ("No"):
-                    return ("You ignore the noise and continue walking.\nWait a second...\nIs that the entrance to Crystal Cliffs Village?\nYou really don't know your way around...")
-                else:
+                        else:
+                            ("That is not a proper response.")
+                        weapon = input("Which weapon will you select for this battle?")
+                        if weapon == "Wooden Broom":
+                            print("Starting fight...")
+                        else:
+                            ("That is not in your inventory.")
+    def fight(self):
+
+
+        elif investigate == ("No"):
+    return ("You ignore the noise and continue walking.\nWait a second...\nIs that the entrance to Crystal Cliffs Village?\nYou really don't know your way around...")
+    else:
                     return("That is not a proper response.")
         elif wickery_hills == ("No"):
                 return("You continue walking.\nHuh...you really don't remember this place...\nWait is that the entrance to Crystal Cliffs Village?")
         else:
-                return ("That is not a proper response.")
-    elif roadsChoice == ("Right"): ##the indentition will make me do something bad.
-return ("You head up the path towards Crystal Cliffs Village.")
-        else:
-            return ("That is not a proper response.")
+                return ("That is not a proper response.")    
+                elif roadsChoice == ("Right"):
+                return ("You head up the path towards Crystal Cliffs Village.")
+else:
+return ("That is not a proper response.")
     
     def battleresult(self):
         if self.health <= 0:
