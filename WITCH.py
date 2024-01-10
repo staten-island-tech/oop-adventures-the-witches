ce725044bbfdcc1e13a4a17b1e560cee8946f8ff
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-01-10 13:57:28 -0500

    worked

--- WITCH.py
+++ WITCH.py
@@ -3,37 +3,58 @@ class Witch:
         self.name = name
         self.health = health
         self.weapon = weapon
-    def start():
+    def start(self):
         print("After the rain had cleared, you were back on your journey...")
         return("Days later you have arrived at the Witch's Coven...\nYou head in, and there's the witch...waiting for you....\n")
-    def battle(self, MCinventory, MChealth, number):    
+    def battle(self, MCinventory, MChealth, number, MCname):    
         if MCinventory == []:
             print ("Uh oh...you have no weapons...")
             print (f"{self.name} won the battle!")
             print (f"{self.name}: Of course I would win...")
             return (f"YOU LOST THE FINAL BATTLE!")
         weapon = input(f"Which weapon would you like to equip from your inventory?\n{MCinventory}")
-        if weapon == ("Wooden Cleaning Broom") or ("Wooden Cane") or ("Knife"):
-            input("What move do you do? (Slash, Stab) ")
+        if weapon == "Wooden Cleaning Broom" \
+        or "Wooden Cane" \
+        or "Knife":
+            move = input("What move do you do? (Slash, Stab) ")
             print (f"{self.name} broke your weapon in half!!\n{self.name} won the battle!\n{self.name}: Of course I would win...")
             return (f"YOU LOST THE FINAL BATTLE!")
         elif weapon == ("Tommy Gun"):
             return(f"Before you could even do anything, the witch used her staff and crushed your weapon!!\n{self.name} won the battle!\n{self.name}: Of course I would win...")
-        elif weapon == ("Sword of Light") or ("Gold Sword"):
+        elif weapon == "Sword of Light"\
+        or "Gold Sword"\
+        or "Gold Bow":
             while self.health>0 and MChealth>0:
                 move = input("What move do you do? (Slash, Stab, Throw) ")
                 if move == ("Slash"):
                     print ("You run forward and slash the witch.")
                     newhealth = self.health-500
                     newhealth = self.health
-                    print (f"Witch: {self.health-500}")
-                    if number == ("one"):
-                        print ("The witch casts a dark spell on you...")
-                        print (f"You: {MChealth-100}")
-                    elif number == ("two"):
-                        print ("The witch uses her staff to expel you backwards...")
-        elif weapon == ("Gold Bow"):
-            print ()
+                    print (f"Witch health: {newhealth}")
+                elif move == ("Stab"):
+                    print ("You stab the witch.")
+                    newhealth = self.health-1000
+                    newhealth = self.health
+                    print (f"Witch health: {newhealth}")
+                elif move == ("Throw"):
+                    print ("You throw your weapon at the witch.")
+                    newhealth = self.health-2000
+                    newhealth = self.health
+                    print (f"Witch health: {newhealth}")
+                else:
+                    return ("That is not a move. ")
+                if number == ("one"):
+                    print ("The witch casts a dark spell on you...")
+                    MChealth = MChealth-800
+                    print (f"{MCname}: {MChealth}")
+                elif number == ("two"):
+                    print ("The witch uses her staff to expel you backwards...")
+                    MChealth = MChealth-500
+                    print (f"{MCname}: {MChealth}")
+                elif number == ("three"):
+                    print ("The witch uses her staff to hit you, as you are in close range.")
+                    MChealth = MChealth-300
+                    print (f"{MCname}: {MChealth}")
         else:
             return ("That's not a proper response.")
     def result(self):
@@ -45,4 +66,3 @@ class Witch:
             print (f"{self.name} won the battle!")
             print (f"{self.name}: Of course I would win...")
             return (f"YOU LOST THE FINAL BATTLE!")
-        
