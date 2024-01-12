14d61b56d6ad74bda542755e0c0b99daef4920fb
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-01-12 13:28:51 -0500

    worked

--- classes/hero.py
+++
@@ -1,8 +0,0 @@
-class Hero:
-    def __init__(self,name, money, inventory):
-        self.name = name
-        self.money = money
-        self.inventory = inventory
-    def buy (self,item):
-        self.inventory.append(item)
-        print(self.inventory)
